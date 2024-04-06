91b37ac55208f97c0384d43d0fc15d765ddeddb0
brad@smithwebtek.com <brad@smithwebtek.com>
2024-04-06 13:00:09 -0400

    Move card to PictureCard component, add tests

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import "./App.css";
 import {
   AppBar,
   Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
+  Container,
   CssBaseline,
   Grid,
   Toolbar,
   Typography,
-  Container,
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
-import useStyles from "./styles";
+import PictureCard from "../src/components/PictureCard/PictureCard";
+import useStyles from "./appStyles";
 
 const App = () => {
   const classes = useStyles();
@@ -68,30 +65,12 @@ const App = () => {
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content
-                    </Typography>
-                    <CardActions>
-                      <Button size="small" color="primary">
-                        View
-                      </Button>
-                      <Button size="small" color="primary">
-                        Edit
-                      </Button>
-                    </CardActions>
-                  </CardContent>
-                </Card>
+                <PictureCard
+                // description={"App description"}
+                // heading={"App picture card Heading"}
+                // image={}
+                // imageTitle='imageTitle from App'
+                />
               </Grid>
             ))}
           </Grid>
